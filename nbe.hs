18966c48b4752727b991a6a0ae1ad26f5b568394
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2025-05-17 23:36:55 +0300

    Реализация на nbe с генериране на свежи променливи като страничен ефект

--- nbe.hs
+++ nbe.hs
@@ -1,15 +1,17 @@
 import Data.Unique
+import Data.Maybe
 
 data T = TVar String | T :=> T
   deriving (Eq, Show, Read, Ord)
 
 ti = TVar "alpha" :=> TVar "alpha"
+
 tc = ti :=> ti
 
 data L = Var String | L :@ L | Abs String L
   deriving (Eq, Show, Read, Ord)
 
-data H = Base { var :: L } | Fun { fun :: H -> H }
+data H = Base { var :: L } | Fun { fun :: H -> IO H }
 
 -- fun (Fun f) = f
 
@@ -28,27 +30,39 @@ cplus = Abs "m" $ Abs "n" $ Abs "f" $ Abs "x" ((Var "m" :@ Var "f") :@ ((Var "n"
 genSym :: String -> IO String
 genSym name = (name++) . show . hashUnique <$> newUnique
 
-type Valuation = String -> H
+type Valuation = String -> IO H
 
 modify :: Valuation -> String -> H -> Valuation
 modify xi x a y
- | x == y = a
+ | x == y = return a
  | x /= y = xi y
 
-evaluate :: L -> Valuation -> H
+evaluate :: L -> Valuation -> IO H
 evaluate (Var x)    xi = xi x
-evaluate (m1 :@ m2) xi = fun (evaluate m1 xi) $ evaluate m2 xi
-evaluate (Abs x n)  xi = Fun $ \a -> evaluate n $ modify xi x a
+evaluate (m1 :@ m2) xi = do
+  a <- evaluate m2 xi
+  f <- evaluate m1 xi
+  fun f a
+evaluate (Abs x n)  xi = return $ Fun $ \a -> evaluate n $ modify xi x a
 
-reflect :: T -> L -> H
-reflect (TVar _) m = Base m
-reflect (r :=> s) m = Fun (\a -> reflect s (m :@ reify r a))
+reflect :: T -> L -> IO H
+reflect (TVar _) m = return $ Base m
+reflect (r :=> s) m = return $ Fun (\a -> do n <- reify r a
+                                             reflect s (m :@ n))
 
-reify :: T -> H -> L
-reify (TVar _) (Base m) = m
-reify (r :=> s) (Fun f) = Abs x (reify s (f (reflect r (Var x))))
-  where x = "x"
+reify :: T -> H -> IO L
+reify (TVar _) (Base m) = return m
+reify (r :=> s) (Fun f) =
+  do
+    x <- genSym "x"
+    a <- reflect r (Var x)
+    b <- f a
+    c <- reify s b
+    return $ Abs x c
 
-nbe :: T -> L -> L
-nbe t m = reify t (evaluate m error)
+nbe :: T -> L -> IO L
+nbe t m = do a <- evaluate m error
+             reify t a
 
+skk = nbe ti $ s :@ k :@k
+c8  = nbe tc $ cplus :@ (c 3) :@ (c 5)
